fed47afd3ddb82582c40d07339b2b900c718a120
BrendanZhang <ex.arc.zhang@gmail.com>
2019-06-12 02:33:09 +0800

    改变多文件上传

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const express = require("express")
-const cors = require("cors")
-const multer = require("multer")
-const upload = multer({ dest: "uploads/" })
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-const app = express()
+const app = express();
 
 app.get("/", (req, res) => {
-	res.send("hello express")
-})
-app.get("/upload", cors(), upload.single("file"), (req, res) => {
-	let filename = req.file.filename
-	let object = { id: filename }
-	res.send(JSON.stringify(object))
-})
-app.post("/preview/:key", cors(), (req, res) => {
-	res.sendFile(
-		`uploads/${req.params.key}`,
-		{
-			root: __dirname,
-			headers: {
-				"Content-Type": "image/jpeg",
-			},
-		},
-		(error) => {
-			console.log(error)
-		},
-	)
-})
-var port = process.env.PORT || 3000
-console.log(port)
-app.listen(port)
+  res.send("hello express");
+});
+app.options("/upload", cors());
+app.post("/upload", cors(), upload.single("file"), (req, res) => {
+  let filename = req.file.filename;
+  console.log(filename);
+  let object = { id: filename };
+  res.send(JSON.stringify(object));
+});
+app.get("/preview/:key", cors(), (req, res) => {
+  res.sendFile(
+    `uploads/${req.params.key}`,
+    {
+      root: __dirname,
+      headers: {
+        "Content-Type": "image/jpeg"
+      }
+    },
+    error => {
+      console.log(error);
+    }
+  );
+});
+var port = process.env.PORT || 3000;
+console.log(port);
+app.listen(port);
